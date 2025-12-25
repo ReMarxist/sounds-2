41c907a9ccc670b8b8ef1f18c03cfd31d2efce54
ReMarxist <artem-kokorin@yandex.ru>
2025-12-25 23:04:32 +0300

    Add pointer out

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3');
   audio.load();
 
-function play() {
+function play(event) {
   audio.play();
+  document.getElementById("status").textContent = "play";
+
+  event.preventDefault();
 }
 
 function stop_sound() {
   audio.pause();
   audio.load();
+  document.getElementById("status").textContent = "stop";
 }
